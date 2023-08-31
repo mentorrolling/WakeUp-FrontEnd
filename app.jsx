26b84f15be006671414fdbe3884a401c5518cd2d
lar159 <luu.rivadeneira@gmail.com>
2023-08-31 19:29:15 -0300

    Installacion dependencies and scaffolding done

---
+++ app.jsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import  { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/components/pages/Home/Home";
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter> 
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
