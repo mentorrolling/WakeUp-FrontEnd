c04983bb8ae3765101c4a55b01055135be001223
lar159 <luu.rivadeneira@gmail.com>
2023-09-06 11:20:18 -0300

    changes in home branch

---
+++ router/routerprincipal.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import Home from '../componentes/pages/home/Home'
+// import Orders  from '../componentes/pages/Orders/Orders'
+// import { Route, Routes } from 'react-router-dom'
+
+// export const routerprincipal = () => {
+  
+//     <Routes>
+//         <Route path='/componentes' element= {<Home/>}/>
+//         <Route path='/componentes' element= {<Orders/>}/>
+//     </Routes>
+  
+// }
---
+++ src/componentes/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import "./Home.css"
+
+function Home() {
+
+
+  return (
+    <>
+      <div className='home-container'>
+
+        <h1 className='title'>Welcome Coffee WakeUp! </h1>
+        <h2>Somos una empresa de cafe gourmet Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, doloribus commodi molestias fuga cum illo vero, suscipit explicabo harum iste corporis deleniti similique voluptatum dicta ullam vitae, excepturi nostrum et.</h2>
+      </div>
+      <br />
+      <div>
+        <h5>El cafe mas elegido de la semana Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ratione voluptates atque obcaecati delectus quos, et dolorum tenetur non quis, molestiae porro facere minus vero, quasi magni! Modi, eligendi est!</h5>
+      <img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="cafe elegido" />
+      </div>
+      <div>
+        <h2 className='text-center'>Menu</h2>
+        <h3>Menú de Cafetería y Pastelería</h3>
+   
+      </div>
+
+    </>
+  )
+}
+export default Home
+
+
