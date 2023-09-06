59d00a0e6479bf3320320b55cfc788645e84207f
lar159 <luu.rivadeneira@gmail.com>
2023-09-06 17:31:36 -0300

    changes in home branch

--- src/componentes/pages/Home/Home.jsx
+++ src/componentes/pages/Home/Home.jsx
@@ -1,28 +1,81 @@
 import React from "react"
 import "./Home.css"
+import { Row, Col } from "react-bootstrap";
+import "../../../img/001.jpg"
+
 
 function Home() {
 
 
   return (
-    <>
-      <div className='home-container'>
+    <div>
+      <div className="header-container">
+        <Row>
+          <Col><h1 className="title">WakeUp! Coffee </h1></Col>
+          <Col> <img src="../../../img/001.jpg" alt="cafe" /></Col>
+        </Row>
+      </div>
+      <div className="col m-3 col-0">
+        <Row>
+          <Col> <img src="../../../img/001.jpg" alt="taza de cafe" /> </Col>
+          <Col> <h4>Somos una empresa de cafe gourmet Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, doloribus commodi molestias fuga cum illo vero, suscipit explicabo harum iste corporis deleniti similique voluptatum dicta ullam vitae, excepturi nostrum et.</h4>
+         </Col>
+        </Row>
 
-        <h1 className='title'>Welcome Coffee WakeUp! </h1>
-        <h2>Somos una empresa de cafe gourmet Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, doloribus commodi molestias fuga cum illo vero, suscipit explicabo harum iste corporis deleniti similique voluptatum dicta ullam vitae, excepturi nostrum et.</h2>
       </div>
       <br />
+      <br />
       <div>
-        <h5>El cafe mas elegido de la semana Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ratione voluptates atque obcaecati delectus quos, et dolorum tenetur non quis, molestiae porro facere minus vero, quasi magni! Modi, eligendi est!</h5>
-      <img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="cafe elegido" />
+        <Row>
+          <Col><h5>El cafe mas elegido de la semana Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ratione voluptates atque obcaecati delectus quos, et dolorum tenetur non quis, molestiae porro facere minus vero, quasi magni! Modi, eligendi est!</h5></Col>
+          <Col><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="cafe elegido" /></Col>
+        </Row>
+        
       </div>
-      <div>
-        <h2 className='text-center'>Menu</h2>
-        <h3>Menú de Cafetería y Pastelería</h3>
-   
+      <br />
+      <div className="menu-color">
+        <Row>
+          <Col><h2 className="text-center">Menu</h2></Col>
+        </Row>
+        <Row>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+          <Col className="centrado"><img src="../../../img/how-to-make-a-macchiato-003s.webp" alt="img" />
+          <h3>Americano</h3>
+          <p>csdsdfds</p>
+          <p className="price">{20}</p>
+          </Col>
+        </Row>
       </div>
 
-    </>
+    </div>
   )
 }
 export default Home
