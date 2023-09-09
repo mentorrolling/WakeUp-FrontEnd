19dc86b6963a30a124b02d6f99fd3ba168d4bfaf
Figueroa Ian Augusto <figueroa.ian13@gmail.com>
2023-09-08 21:25:33 -0300

    Se creó interfaz, con un Registro y un Login dinámico; con ellos su respectivos JS y CSS.

--- registerAndLogin/JAVAregLogin.js
+++ registerAndLogin/JAVAregLogin.js
@@ -1,5 +1,22 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
 let formRegistro = document.getElementById("formRegistro")
+let formLogin = document.getElementById("formLogin")
 
 formRegistro.addEventListener("submit", (event) => {
     event.preventDefault();
 })
+
+formLogin.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
