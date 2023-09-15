170cac18e9515d8e3adfcf9cae74ed996a978916
Agustín Dominguez <131421006+AgusDomz@users.noreply.github.com>
2023-09-14 22:14:23 -0300

    change footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,72 +1,131 @@
 import { FaXTwitter } from "react-icons/fa6";
-import { FiGithub } from 'react-icons/fi'
-import { FaLinkedinIn, FaFacebookF, FaGoogle, FaInstagram } from 'react-icons/fa'
+import { FiGithub } from 'react-icons/fi';
+import { FaLinkedinIn, FaFacebookF, FaGoogle, FaInstagram } from 'react-icons/fa';
+import { BiHomeAlt2, BiInfoCircle } from 'react-icons/bi';
+import { BsTelephone } from 'react-icons/bs';
+import { AiOutlineHeart } from "react-icons/ai";
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="text-center text-black" style={{ backgroundColor: '#f2d3bf' }}>
-      <div className="container">
-        <section className="mt-3">
-          <div className="row text-center d-flex justify-content-center pt-3"> 
-            <div className="col-md-2">
-              <h6 className="text-uppercase font-weight-bold">
-                <a href="#!" className="text-black text-decoration-none">About us</a>
+    <footer className="text-center text-lg-start text-black">
+      <div className="container p-3 pb-0">
+        <section className="">
+          <div className="row">
+            <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-3 font-weight-bold">
+                WakeUp!-Coffe
               </h6>
-            </div>
-            <div className="col-md-2">
-              <h6 className="text-uppercase font-weight-bold">
-                <a href="#!" className="text-black text-decoration-none">Menús</a>
-              </h6>
-            </div>
-            <div className="col-md-2">
-              <h6 className="text-uppercase font-weight-bold">
-                <a href="#!" className="text-black text-decoration-none">Help</a>
-              </h6>
-            </div>
-            <div className="col-md-2">
-              <h6 className="text-uppercase font-weight-bold">
-                <a href="#!" className="text-black text-decoration-none">Contact</a>
-              </h6>
-            </div>
-          </div>
-        </section>
-        <hr className="my-3" /> 
-        <section className="mb-3"> 
-          <div className="row d-flex justify-content-center">
-            <div className="col-lg-8">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                distinctio earum repellat quaerat voluptatibus placeat nam,
-                commodi optio pariatur est quia magnam eum harum corrupti
-                dicta, aliquam sequi voluptate quas.
+                Experience pure coffee bliss at WakeUp! Coffee Shop, where passion meets quality in every cup. Explore our aromatic world of freshly roasted beans, served with love from the heart of the city.
               </p>
             </div>
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-3 font-weight-bold ">Products</h6>
+              <p>
+                <a className="text-black text-decoration-none">Coffe</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none">Croassaints</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none">Cakes</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none">Sandwiches</a>
+              </p>
+            </div>
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-3 font-weight-bold">
+                Creators
+              </h6>
+              <p>
+                <a className="text-black text-decoration-none"><AiOutlineHeart /> Agus Dominguez</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none"><AiOutlineHeart /> Lu Rivadeneira</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none"><AiOutlineHeart /> Ian Figueroa</a>
+              </p>
+              <p>
+                <a className="text-black text-decoration-none"><AiOutlineHeart /> Gus Andina</a>
+              </p>
+            </div>
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-3 font-weight-bold">Contact</h6>
+              <p><BiHomeAlt2 /> Tucumán, SM 4000, ARG</p>
+              <p><BiInfoCircle /> HelloWorldCompany@gmail.com</p>
+              <p><BsTelephone /> +01 234 567 89</p>
+            </div>
           </div>
         </section>
-        <section className="text-center mb-3"> 
-          <a href="https://www.facebook.com/" className="text-black me-3">
-            <FaFacebookF className="facebook" />
-          </a>
-          <a href="https://twitter.com/" className="text-black me-3">
-            <FaXTwitter />
-          </a>
-          <a href="https://www.google.com/" className="text-black me-3">
-            <FaGoogle />
-          </a>
-          <a href="https://www.instagram.com/" className="text-black me-3">
-            <FaInstagram />
-          </a>
-          <a href="https://www.linkedin.com/" className="text-black me-3">
-            <FaLinkedinIn />
-          </a>
-          <a href="https://github.com/" className="text-black me-3">
-            <FiGithub />
-          </a>
+
+        <hr className="my-3" />
+
+        <section className="p-2 pt-0">
+          <div className="row d-flex align-items-center">
+            <div className="col-md-7 col-lg-8 text-center text-md-start">
+              <div className="p-2">
+                © 2023 Copyright:
+                <a className="text-black text-decoration-none">
+                  HelloWorldCompany
+                </a>
+              </div>
+            </div>
+
+            <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FaFacebookF />
+              </a>
+
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FaXTwitter />
+              </a>
+
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FaGoogle />
+              </a>
+
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FaLinkedinIn />
+              </a>
+              <a
+                className="btn btn-outline-dark m-1 rounded-circle"
+                role="button"
+              >
+                <FiGithub />
+              </a>
+            </div>
+          </div>
         </section>
-      </div>
-      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2023 Copyright:
-        <a className="text-black text-decoration-none"> HelloWorldCompany</a>
       </div>
     </footer>
   );
