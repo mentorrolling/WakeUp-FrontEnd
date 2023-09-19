4e0a1246aa3e3e519eeca84d43e06d29d1e5091f
pmarino2013 <pmarino2013@gmail.com>
2023-09-19 18:46:53 -0300

    Quitar exact de las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Navbar from "./components/layout/NavBar/NavBar";
 import Footer from "./components/layout/Footer/Footer";
 import Home from "./components/pages/Home/Home";
-import Orders from "./components/pages/Orders/Orders"
+import Orders from "./components/pages/Orders/Orders";
 import Error404 from "./components/pages/Error404/Error404";
 import Menu from "./components/pages/Menu/Menu";
 import Dashboard from "./components/pages/AdminDashboard/Dashboard";
-
 
 function App() {
   const isDashboardRoute = window.location.pathname.startsWith("/dashboard");
@@ -17,17 +16,16 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/product/orders" element={<Orders />} />
-          <Route exact path="/*" element={<Error404 />} />
-          <Route exact path="/menu" element={<Menu />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product/orders" element={<Orders />} />
+          <Route path="/*" element={<Error404 />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
         {isDashboardRoute ? <Outlet /> : <Footer />}
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
-
